6f720032cec7e2fa00e9285a76a9173d8a4a86cf
danvc95 <dinovc@icloud.com>
2023-05-16 23:01:14 -0700

    Added new images

---
+++ Kaizen Fitness/Kaizen Fitness/Kaizen_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaizen_FitnessApp.swift
+//  Kaizen Fitness
+//
+//  Created by Daniel Van Cleave on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Kaizen_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
